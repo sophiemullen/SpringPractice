4a53b395c242af31ddeae097d79aa9902eb95751
Sophie Mullen <sophie.mullen@cegeka.com>
2019-02-03 00:08:03 +0100

    Annotations: Bean scopes and lifecycle

--- SpringDemoAnnotations/src/annotations/springdemocoach/PersonalTrainer.java
+++ SpringDemoAnnotations/src/annotations/springdemocoach/PersonalTrainer.java
@@ -3,9 +3,11 @@ package annotations.springdemocoach;
 import annotations.springdemocoach.service.FortuneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class PersonalTrainer implements Coach {
 
     private FortuneService fortuneService;
--- SpringDemoAnnotations/src/annotations/springdemocoach/RugbyCoach.java
+++ SpringDemoAnnotations/src/annotations/springdemocoach/RugbyCoach.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class RugbyCoach implements Coach {
 
@@ -17,6 +20,16 @@ public class RugbyCoach implements Coach {
     @Autowired
     public void setFortuneService(@Qualifier("happyFortuneService") FortuneService fortuneService) {
         this.fortuneService = fortuneService;
+    }
+
+    @PostConstruct
+    public void doOnInit(){
+        System.out.println("Inside rugbyCoach init method");
+    }
+
+    @PreDestroy
+    public void doOnDestroy(){
+        System.out.println("Inside rugbyCoach destroy method");
     }
 
     @Override
