688e707081e71f8893a9c4ecabc4f46773ddd1e7
Sophie Mullen <sophie.mullen@cegeka.com>
2019-03-20 00:06:41 +0100

    Spring MVC: adding @Initbinder to trim whitespaces out of the string

--- SpringMVC/src/main/java/springdemocoach/CustomerController.java
+++ SpringMVC/src/main/java/springdemocoach/CustomerController.java
@@ -1,8 +1,11 @@
 package springdemocoach;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,5 +31,12 @@ public class CustomerController {
         } else {
             return "customer-confirmation";
         }
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
